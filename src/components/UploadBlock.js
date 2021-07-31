c553cf802a541160a25c7a0a609d1baecb8adb85
Ehsan Munna <ehsanul@cokreates.com>
2021-07-31 12:37:02 +0600

    upload component added and use to app.js

---
+++ src/components/UploadBlock.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UploadBlock = () => {
+  return (
+    // <>
+    //   <select data-testid="select-subject">
+    //   </select>
+      
+    // </>
+    
+    <div>
+      upload
+  </div>
+  )
+}
+
+export default UploadBlock;
