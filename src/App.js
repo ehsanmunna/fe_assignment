2ad2e143b279b7bb48e1d354613e322ff668627d
Ehsan Munna <ehsanul@cokreates.com>
2021-07-31 12:07:02 +0600

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react'
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='container'>
+        <div className="row">
+          <div className="col-2">sidebar</div>
+          <div className="col-10">dashboard</div>
+        </div>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Before getting started please go through the FE Assignment Instruction.pdf</p>
         <p>Do not alter the given mock tempering with it won't give the required test results</p>
@@ -12,7 +20,7 @@ function App() {
         <p>Reuse the test-id tagged elements and the given mock data for developing the UI elements</p>
         <p>Keep all your UI elements under the components folder</p>
         <p>You can add additional test cases to increase the code coverage</p>
-      </header>
+      </header> */}
     </div>
   );
 }
